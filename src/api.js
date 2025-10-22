50d25e2efe9664266a476c684a94d593a2476665
lizmint <lizagunina21@gmail.com>
2025-10-22 17:22:40 +0300

    Get data

---
+++ src/api.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import convert from 'xml-js';
+
+import { WGS84_a, ZULUGIS_ZWS_URL } from './constants.js';
+
+export const getPointData = async (coords, layer, zoom) => {
+  const scale = 2 * Math.PI * WGS84_a / (256 * (1 << zoom))
+
+  let xml= `<?xml version="1.0" encoding="UTF-8"?>
+    <zulu-server service='zws' version='1.0.0'>
+      <Command>
+        <SelectElemByXY>
+          <Layer>" + layer + "</Layer>
+          <X>" + ${coords[0]} + "</X>
+          <Y>" + ${coords[1]} + "</Y>
+          <Scale>" + ${scale} + "</Scale>
+          <CRS>'EPSG:4326'</CRS>
+        </SelectElemByXY>
+      </Command>;
+    </zulu-server>`;
+
+  return axios.post(ZULUGIS_ZWS_URL,
+    xml,
+    {
+      headers: { 'Content-Type': 'text/xml' },
+    }).then(res=> {
+      const fullData = convert.xml2js(res, { compact: true, spaces: 2 });
+      const fields = fullData?.zwsResponse?.SelectElemByXY?.Element?.Records?.Record?.Field;
+
+      return fields?.map(field => ({
+        name: field.UserName?._text,
+        value: field.Value?._text
+      }));
+    })
+}
--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
-
 export const ZULUGIS_ZWS_URL = 'http://zs.zulugis.ru:6473/zws';
+
+export const WGS84_a = 6378137.0;
